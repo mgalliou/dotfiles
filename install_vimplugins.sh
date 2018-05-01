bec07919e02621e14fd1acdce5d1ba56f8fc0aa8
mgalliou <matthias.galliou@gmail.com>
2018-05-01 10:02:34 +0200

    update install_vimplugins script

--- install_vimplugins.sh
+++ install_vimplugins.sh
@@ -1,6 +1,8 @@
+mkdir -p ~/dotfiles/vim/backup/undo
 cd ~/dotfiles/vim/bundle
 git clone https://github.com/tpope/vim-pathogen.git
 git clone https://github.com/felixhummel/setcolors.vim.git
 git clone https://github.com/flazz/vim-colorschemes.git
 git clone https://github.com/tpope/vim-repeat.git
 git clone https://github.com/tpope/vim-surround.git
+cd -
