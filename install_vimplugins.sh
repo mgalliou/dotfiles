234844787181185957ff8ea44bee995ca3f1832f
mgalliou <matthias.galliou@gmail.com>
2018-12-09 04:42:33 +0100

    vim: update install script

--- install_vimplugins.sh
+++ install_vimplugins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 mkdir -p ~/dotfiles/vim/backup/undo
+mkdir -p ~/dotfiles/vim/bundle
 cd ~/dotfiles/vim/bundle
 git clone https://github.com/tpope/vim-pathogen.git
 git clone https://github.com/felixhummel/setcolors.vim.git
