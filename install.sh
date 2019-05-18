38c07c81abd8b1ba3d3b3be537e63fbe3e9a1a40
mgalliou <matthias.galliou@gmail.com>
2019-05-18 14:24:42 +0200

    install.sh: create directories before symlinking

--- install.sh
+++ install.sh
@@ -11,13 +11,16 @@ symlink_dotfile()
 	flush_stdin
 	read CREATE
 	if [ "$CREATE" = "y" ]; then
-		#link
+		mkdir -pv ~/.$(dirname $1)
 		ln -sv ~/dotfiles/$1 ~/.$1
 	fi
 }
 
 #BASH SETUP
 symlink_dotfile "bashrc"
+
+#FISH SETUP
+symlink_dotfile "config/fish/config.fish"
 
 #TMUX SETUP
 symlink_dotfile "tmux.conf"
@@ -31,7 +34,7 @@ if [ "$SETUPVIM" = "y" ]; then
 	symlink_dotfile vimrc
 	symlink_dotfile vim
 	symlink_dotfile config/nvim/init.vim
-	mkdir -vp ~/dotfiles/vim/backup/undo ~/dotfiles/vim/bundle
+	mkdir -pv ~/dotfiles/vim/backup/undo ~/dotfiles/vim/bundle
 	echo "Created backup/undo and bundle directories"
 fi
 
@@ -40,16 +43,17 @@ symlink_dotfile "Xresources"
 symlink_dotfile "xinitrc"
 symlink_dotfile "xmodmap"
 
+#TERMITE
+symlink_dotfile "config/termite/config"
+
 #I3 SETUP
-flush_stdin
 echo -n "setup i3? (y/n)"
+flush_stdin
 read SETUPI3
 if [ "$SETUPI3" = "y" ]; then
 	echo "[I3 SETUP]"
-	mkdir -p ~/.config/i3 ~/.config/i3status
+	mkdir -pv ~/.config/i3 ~/.config/i3status
 	symlink_dotfile "config/i3/config"
 	symlink_dotfile "config/i3status/config"
 fi
 
-#FISH SETUP
-symlink_dotfile "config/fish/config.fish"
