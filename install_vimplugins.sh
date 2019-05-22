969eb898f23ffaad57fb425411d5fdb72df3dc8c
mgalliou <matthias.galliou@gmail.com>
2019-05-22 15:17:53 +0200

    vim: improve plugin install script

--- install_vimplugins.sh
+++ install_vimplugins.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
 PLUGINS_PATH=~/dotfiles/vim/pack/plugins/start
-mkdir -p ~/dotfiles/vim/backup/undo
-mkdir -p ${PLUGINS_PATH}
+mkdir -pv ~/dotfiles/vim/backup/undo
+mkdir -pv ${PLUGINS_PATH}
 cd ${PLUGINS_PATH}
-pwd
-git clone https://github.com/felixhummel/setcolors.vim.git
-#git clone https://github.com/flazz/vim-colorschemes.git
-git clone https://github.com/tpope/vim-repeat.git
-git clone https://github.com/tpope/vim-surround.git
-#linter
-git clone https://github.com/w0rp/ale
-#autocompletion
-git clone https://github.com/Shougo/deoplete.nvim.git
-#syntax
-git clone https://github.com/leafgarland/typescript-vim.git
-git clone https://github.com/cespare/vim-toml.git
-git clone https://github.com/dag/vim-fish.git
-#brackets
-git clone https://github.com/luochen1990/rainbow
+repos=(https://github.com/felixhummel/setcolors.vim
+	#"https://github.com/flazz/vim-colorschemes.git"
+	https://github.com/tpope/vim-repeat
+	https://github.com/tpope/vim-surround
+	#linter
+	https://github.com/w0rp/ale
+	#autocompletion
+	https://github.com/Shougo/deoplete.nvim
+	#syntax
+	https://github.com/leafgarland/typescript-vim
+	https://github.com/cespare/vim-toml
+	https://github.com/dag/vim-fish
+	#brackets
+	https://github.com/luochen1990/rainbow
+	#git integration
+	https://github.com/airblade/vim-gitgutter)
+for repo in "${repos[@]}"
+do
+	git clone $repo
+done
 cd -
