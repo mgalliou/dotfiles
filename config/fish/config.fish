82751ac124a4aa9c272f4180301cb24d5d91582c
mgalliou <matthias.galliou@gmail.com>
2019-05-23 11:10:29 +0200

    fish: fix aliases when command does not exists

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,17 +1,23 @@
 fish_vi_key_bindings
 
+function fish_mode_prompt
+# NOOP - Disable vim mode indicator
+end
+
 set -U fish_color_cwd white
 set -U fish_color_host brgrey
 set -U fish_color_user brblue
 set -U fish_greeting ""
 
-alias ls exa
-alias cat bat
-alias vim nvim
-
-function fish_mode_prompt
-# NOOP - Disable vim mode indicator
+function replace_cmd
+	if type -q $argv[2]
+		alias $argv[1] $argv[2]
+	end
 end
+
+replace_cmd ls exa
+replace_cmd cat bat
+replace_cmd vim nvim
 
 function fish_prompt --description 'Write out the prompt'
 	set -l last_status $status
