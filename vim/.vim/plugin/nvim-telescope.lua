2a0825b4d17472f1f9357e2638ddadb0d29f5a5f
mgalliou <matthias.galliou@gmail.com>
2022-10-24 18:06:42 +0200

    move and edit telescope mapping to lua file

--- vim/.vim/plugin/nvim-telescope.lua
+++ vim/.vim/plugin/nvim-telescope.lua
@@ -1,1 +1,7 @@
 require('telescope').setup{}
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', function() builtin.find_files({hidden=true}) end, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
