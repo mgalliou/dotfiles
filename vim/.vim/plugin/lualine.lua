e2ab49c07d1e4fe4360502a0d3581cd1cb793bc4
mgalliou <matthias.galliou@gmail.com>
2022-11-07 17:00:20 +0100

    nvim: configure sections

--- vim/.vim/plugin/lualine.lua
+++ vim/.vim/plugin/lualine.lua
@@ -1,5 +1,13 @@
 require('lualine').setup {
 	options = {
 		theme = 'gruvbox'
+	},
+	sections = {
+		lualine_a = {'mode'},
+		lualine_b = {'branch', 'diff', 'diagnostics'},
+		lualine_c = {'filename'},
+		lualine_x = {},
+		lualine_y = {'filetype'},
+		lualine_z = {'progress'}
 	}
 }
