a52cd0364332a60a9e549b57e33bbc4ef0c4d6e2
mgalliou <matthias.galliou@gmail.com>
2022-11-15 21:37:52 +0100

    nvim: telescope: refactor and edit find_files picker setup

--- vim/.vim/plugin/nvim-telescope.lua
+++ vim/.vim/plugin/nvim-telescope.lua
@@ -1,7 +1,14 @@
-require('telescope').setup{}
+require('telescope').setup{
+	pickers = {
+		find_files = {
+			theme = "dropdown",
+			hidden = true
+		}
+	}
+}
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', function() builtin.find_files({hidden=true}) end, {})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
