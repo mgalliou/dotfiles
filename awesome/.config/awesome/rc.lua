bac184093d6a8e671d443e4571e0bcc16e7169c5
mgalliou <matthias.galliou@gmail.com>
2022-11-16 15:14:17 +0100

    awesome: remove layout widget from wibar

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -219,7 +219,6 @@ awful.screen.connect_for_each_screen(function(s)
             mykeyboardlayout,
             wibox.widget.systray(),
             mytextclock,
-            s.mylayoutbox,
         },
     }
 end)
