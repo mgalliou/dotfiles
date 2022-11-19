3002b9fb8b04795118a99db8160348baad1f4ab7
mgalliou <matthias.galliou@gmail.com>
2022-11-19 16:02:08 +0100

    nvim: telescope: add find_dotfiles mapping

--- vim/.vim/plugin/nvim-telescope.lua
+++ vim/.vim/plugin/nvim-telescope.lua
@@ -11,7 +11,13 @@ require('telescope').setup {
 }
 
 local builtin = require('telescope.builtin')
+local function find_dotfiles()
+	builtin.find_files {
+		cwd = "~/dotfiles",
+	}
+end
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fd', find_dotfiles, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
