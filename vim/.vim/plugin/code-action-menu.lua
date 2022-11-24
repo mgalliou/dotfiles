461d5ca4570d85dd16611cbad596472e9d4f3702
mgalliou <matthias.galliou@gmail.com>
2022-11-24 10:14:31 +0100

    nvim: enable CodeActionMenu mapping in visual mode

--- vim/.vim/plugin/code-action-menu.lua
+++ vim/.vim/plugin/code-action-menu.lua
@@ -1,2 +1,2 @@
 local bufopts = { noremap = true, silent = true}
-vim.keymap.set('n', '<leader>ca', ":CodeAction<CR>", bufopts)
+vim.keymap.set({'n', 'v'}, '<leader>ca', vim.cmd.CodeActionMenu, bufopts)
