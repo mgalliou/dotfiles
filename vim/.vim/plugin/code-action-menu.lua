93ecbe9362c85dac33cf63c169df0c59dc83aefe
mgalliou <matthias.galliou@gmail.com>
2022-11-25 18:20:40 +0100

    nvim: code-action-menu: disable diff and details

--- vim/.vim/plugin/code-action-menu.lua
+++ vim/.vim/plugin/code-action-menu.lua
@@ -1,2 +1,6 @@
 local bufopts = { noremap = true, silent = true}
+
+vim.g.code_action_menu_show_diff = false
+vim.g.code_action_menu_show_details = false
+
 vim.keymap.set({'n', 'v'}, '<leader>ca', vim.cmd.CodeActionMenu, bufopts)
