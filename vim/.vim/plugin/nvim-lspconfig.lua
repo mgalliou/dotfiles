c566615dd41b007455a6587c9252bea3a798ade6
mgalliou <matthias.galliou@gmail.com>
2022-11-26 09:08:08 +0100

    nvim(lspconfig): add awesome lib to luals workspace

--- vim/.vim/plugin/nvim-lspconfig.lua
+++ vim/.vim/plugin/nvim-lspconfig.lua
@@ -80,7 +80,10 @@ require('lspconfig').sumneko_lua.setup {
 			},
 			workspace = {
 				-- Make the server aware of Neovim runtime files
-				library = vim.api.nvim_get_runtime_file("", true),
+				library = {
+					vim.api.nvim_get_runtime_file("", true),
+                    "/usr/share/awesome/lib",
+				}
 			},
 			-- Do not send telemetry data containing a randomized but unique identifier
 			telemetry = {
