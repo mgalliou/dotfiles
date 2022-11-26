6fc97666e03903fcf8a2cd40ecbddef1fc48d685
mgalliou <matthias.galliou@gmail.com>
2022-11-26 11:34:01 +0100

    nvim(lightbulb): customize setup

--- vim/.vim/plugin/nvim-lightbulb.lua
+++ vim/.vim/plugin/nvim-lightbulb.lua
@@ -1,1 +1,16 @@
-require('nvim-lightbulb').setup({autocmd = {enabled = true}})
+require('nvim-lightbulb').setup {
+	sign = {
+		enabled = false,
+	},
+	virtual_text = {
+		enabled = true,
+		hl_mode = "combine",
+	},
+	status_text = {
+		enabled = true,
+	},
+	autocmd = {
+		enabled = true,
+		events = { "CursorMoved", "CursorMovedI" }
+	}
+}
