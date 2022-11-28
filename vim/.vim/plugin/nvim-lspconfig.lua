0e91b6dc5b98f8d9fb364f3b1ac99820d9340bd2
mgalliou <matthias.galliou@gmail.com>
2022-11-28 09:17:43 +0100

    nvim(lspconfig): add quick code action mapping

--- vim/.vim/plugin/nvim-lspconfig.lua
+++ vim/.vim/plugin/nvim-lspconfig.lua
@@ -1,10 +1,23 @@
+local function quick_code_action()
+	vim.lsp.buf.code_action {
+		context = {
+			only = {
+				"quickfix",
+				"refactor",
+			},
+		},
+		apply = true,
+	}
+end
+
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', '<leader>sll', vim.diagnostic.setloclist, opts)
+
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -28,6 +41,7 @@ local on_attach = function(client, bufnr)
 	vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
 	--  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
 	-- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', "<leader>qf", quick_code_action, bufopts)
 	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
 	vim.keymap.set({'n', 'v'}, '<leader>fm', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
