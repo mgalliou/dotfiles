a42f5d5062c3130272df50e0259b202056291ff7
mgalliou <matthias.galliou@gmail.com>
2022-11-28 18:52:46 +0100

    nvim(lspconfig): setup completion kind icons

--- vim/.vim/plugin/nvim-lspconfig.lua
+++ vim/.vim/plugin/nvim-lspconfig.lua
@@ -115,3 +115,32 @@ vim.diagnostic.config {
 	update_in_insert = false,
 	severity_sort = true,
 }
+
+local icons = {
+  Class = " ",
+  Color = " ",
+  Constant = " ",
+  Constructor = " ",
+  Enum = "了 ",
+  EnumMember = " ",
+  Field = " ",
+  File = " ",
+  Folder = " ",
+  Function = " ",
+  Interface = "ﰮ ",
+  Keyword = " ",
+  Method = "ƒ ",
+  Module = " ",
+  Property = " ",
+  Snippet = " ",
+  Struct = " ",
+  Text = " ",
+  Unit = " ",
+  Value = " ",
+  Variable = " ",
+}
+
+local kinds = vim.lsp.protocol.CompletionItemKind
+for i, kind in ipairs(kinds) do
+	kinds[i] = icons[kind] or kind
+end
