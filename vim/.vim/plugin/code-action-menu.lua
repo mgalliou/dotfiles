ef27f1aa657b9c84d0c4a6ab9316de554277fd9a
mgalliou <matthias.galliou@gmail.com>
2022-12-06 09:39:52 +0100

    nvim: replace code-action-menu with telescope-ui-select

--- vim/.vim/plugin/code-action-menu.lua
+++
@@ -1,6 +0,0 @@
-local bufopts = { noremap = true, silent = true}
-
-vim.g.code_action_menu_show_diff = false
-vim.g.code_action_menu_show_details = false
-
-vim.keymap.set({'n', 'v'}, '<leader>ca', vim.cmd.CodeActionMenu, bufopts)
