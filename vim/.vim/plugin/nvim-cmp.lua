c45ea70320e5d36fcc347a20261df4887e192ef7
mgalliou <matthias.galliou@gmail.com>
2023-01-15 17:43:17 +0100

    nvim: add neorg with basic config

--- vim/.vim/plugin/nvim-cmp.lua
+++ vim/.vim/plugin/nvim-cmp.lua
@@ -20,9 +20,10 @@ cmp.setup {
 		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
 	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'luasnip' }, -- For luasnip users.
-		{ name = 'buffer' },
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" }, -- For luasnip users.
+		{ name = "buffer" },
 		{ name = "path" },
+		{ name = "neorg" },
 	})
 }
