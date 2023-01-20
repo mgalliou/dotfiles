368bb9763d39fcc4b6cdfc765248a308187c322c
mgalliou <matthias.galliou@gmail.com>
2023-01-20 13:20:18 +0100

    nvim(null-ls): enable shellharden and jq source

--- vim/.vim/plugin/null-ls.lua
+++ vim/.vim/plugin/null-ls.lua
@@ -4,5 +4,7 @@ null_ls.setup {
     sources = {
 		null_ls.builtins.code_actions.shellcheck,
 		null_ls.builtins.formatting.beautysh,
+		null_ls.builtins.formatting.shellharden,
+		null_ls.builtins.formatting.jq
     },
 }
