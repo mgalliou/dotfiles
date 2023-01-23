8d4564da5e2b22714b4d5962a40cf2ef33f02a65
mgalliou <matthias.galliou@gmail.com>
2023-01-23 17:39:16 +0100

    nvim(neorg): enable dirman module with default config

--- vim/.vim/plugin/neorg.lua
+++ vim/.vim/plugin/neorg.lua
@@ -1,6 +1,14 @@
 require("neorg").setup {
 	load = {
 		["core.defaults"] = {},
+		["core.norg.dirman"] = {
+			config = {
+				workspaces = {
+					work = "~/notes/work",
+					home = "~/notes/home",
+				},
+			},
+		},
 		["core.norg.concealer"] = {},
 		["core.norg.completion"] = {
 			config = { engine = "nvim-cmp" },
