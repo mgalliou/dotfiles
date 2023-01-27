7cb2d06efe24c0706e37d54c303e8dbdb1e86892
mgalliou <matthias.galliou@gmail.com>
2023-01-27 11:47:35 +0100

    nvim(cmp): change confirm mapping to <C-e> and disable abort mapping (for now)

--- vim/.vim/plugin/nvim-cmp.lua
+++ vim/.vim/plugin/nvim-cmp.lua
@@ -30,8 +30,9 @@ cmp.setup {
 		['<C-b>'] = cmp.mapping.scroll_docs(-4),
 		['<C-f>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-e>'] = cmp.mapping.abort(),
-		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+		-- NOTE: seams useless to me
+		-- ['<C-e>'] = cmp.mapping.abort(),
+		['<C-e'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
 	sources = cmp.config.sources({
 		{ name = "nvim_lsp" },
