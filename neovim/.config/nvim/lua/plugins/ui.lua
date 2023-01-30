818ef92202dd921e0e9c74bffda560f87de0b4d7
mgalliou <matthias.galliou@gmail.com>
2023-01-30 22:17:11 +0100

    nvim: migrate gitsign hlargs and todo-comments

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -1,5 +1,7 @@
 return {
 	{ 'asiryk/auto-hlsearch.nvim' },
+	{ 'm-demare/hlargs.nvim' },
+	{ 'folke/todo-comments.nvim' },
 	{ 'kyazdani42/nvim-web-devicons' },
 	{
 		"nvim-lualine/lualine.nvim",
@@ -16,5 +18,36 @@ return {
 				lualine_z = { 'progress' }
 			}
 		},
+	},
+	{
+		'lewis6991/gitsigns.nvim',
+		opts = {
+			on_attach = function(buffer)
+				local gs = package.loaded.gitsigns
+				local function map(mode, l, r, desc)
+					vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+				end
+
+				-- TODO: compare behavior with suggested bind; disable unused
+				-- keybind
+				-- Navigation
+				map("n", "]h", gs.next_hunk, "Next Hunk")
+				map("n", "[h", gs.prev_hunk, "Prev Hunk")
+				-- Actions
+				map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>', "Stage Hunk")
+				map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>', "Reset Hunk")
+				map('n', '<leader>hS', gs.stage_buffer, "Stage Buffer")
+				map('n', '<leader>hu', gs.undo_stage_hunk, "Undo Stage Hunk")
+				map('n', '<leader>hR', gs.reset_buffer, "Reset Buffer")
+				map('n', '<leader>hp', gs.preview_hunk, "Prevew Hunk")
+				map('n', '<leader>hb', function() gs.blame_line({ full=true }) end, "Blame Line")
+				map('n', '<leader>tb', gs.toggle_current_line_blame, "Toggle Blame")
+				map('n', '<leader>hd', gs.diffthis, "Diff This")
+				map('n', '<leader>hD', function() gs.diffthis('~') end, "Diff This ~")
+				map('n', '<leader>td', gs.toggle_deleted, "Toggle Deleted")
+				-- Text object
+				map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>', "GitSigns Select Hunk")
+			end
+		}
 	},
 }
