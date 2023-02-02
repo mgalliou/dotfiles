c8f801d3c25446567d9e9a42d283f4fb0342958f
mgalliou <matthias.galliou@gmail.com>
2023-02-02 17:03:35 +0100

    nvim: finish migration telescope

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,7 @@ return {
 		'nvim-telescope/telescope.nvim',
 		dependencies = {
 			{ "nvim-lua/plenary.nvim" },
-			{ 
+			{
 				"nvim-telescope/telescope-ui-select.nvim",
 				config = function()
 					require("telescope").load_extension("ui-select")
@@ -21,14 +21,33 @@ return {
 				}
 			}
 		},
+		-- TODO: change and add more keybind
 		keys = {
 			{
 				"<leader>ff",
-				function()
-					require("telescope.builtin").find_files()
-				end,
+				function() require("telescope.builtin").find_files() end,
 				desc = "Find files with Telescope",
-			}
+			},
+			{
+				"<leader>fd",
+				function() require("telescope.builtin").find_files({ cwd = "~/dotfiles" }) end,
+				desc = "Find dotfiles with Telescope",
+			},
+			{
+				"<leader>fg",
+				function() require("telescope.builtin").live_grep() end,
+				desc = "Grep in currend directory with Telescope",
+			},
+			{
+				"<leader>fb",
+				function() require("telescope.builtin").buffers() end,
+				desc = "Find buffer with Telescope",
+			},
+			{
+				"<leader>fh",
+				function() require("telescope.builtin").help_tags() end,
+				desc = "Find helptags with Telescope",
+			},
 		},
 	},
 }
