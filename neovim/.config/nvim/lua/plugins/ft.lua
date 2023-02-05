f71548172f79ef0dbe3f3937f76948945e87e13a
mgalliou <matthias.galliou@gmail.com>
2023-02-05 12:26:32 +0100

    nvim: remove `vim-toml` as is it now a builtin

--- neovim/.config/nvim/lua/plugins/ft.lua
+++ neovim/.config/nvim/lua/plugins/ft.lua
@@ -1,8 +1,6 @@
 return {
-	-- TODO: lazy load on filetype; check configuration
 	{ 'dag/vim-fish', ft = "fish"},
 	{ 'tmux-plugins/vim-tmux', ft = "tmux" },
-	{ 'cespare/vim-toml', ft = "toml" },
 	{ 'leafgarland/typescript-vim', ft = "typescript" },
 	{ 'pearofducks/ansible-vim', ft = "yaml" },
 	{ 'towolf/vim-helm', ft = "yaml" },
