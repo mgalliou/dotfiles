867486394859a732e9eeddbdebf63cf7a45ba1af
mgalliou <matthias.galliou@gmail.com>
2023-02-05 11:34:38 +0100

    nvim: add and implement missing options; format

--- neovim/.config/nvim/lua/config/options.lua
+++ neovim/.config/nvim/lua/config/options.lua
@@ -20,6 +20,7 @@ set updatetime=100
 ]] --
 
 -- UI
+vim.opt.termguicolors = true
 vim.opt.showmode = false
 vim.opt.signcolumn = "yes"
 vim.opt.number = true
@@ -27,9 +28,9 @@ vim.opt.relativenumber = true
 vim.opt.wildmenu = true
 vim.opt.colorcolumn = "81"
 vim.opt.cursorline = true
--- TODO: lua implem
---vim.opt.listchars = { "space:·", "eol:↴" }
---runtime macros/justify.vim "allow paragraph justification with _j 
+vim.opt.listchars:append("space:·")
+vim.opt.listchars:append("eol:↴")
+vim.cmd([[runtime macros/justify.vim]]) --allow paragraph justification with `_j`
 vim.opt.cmdheight = 0
 
 -- Searching
@@ -38,8 +39,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.showmatch = true
 
--- TODO: lua implem
---[[
+--[[TODO: lua implem
 if exists("##CmdLineEnter")
 	augroup dynamic\_smartcase
 		autocmd!
@@ -47,7 +47,7 @@ if exists("##CmdLineEnter")
 		autocmd CmdLineLeave : set smartcase
 	augroup END
 endif
-]]--
+]] --
 
 -- Folding
 vim.opt.foldmethod = "marker"
@@ -56,7 +56,7 @@ vim.opt.foldmethod = "marker"
 vim.g.asmsyntax = 'nasm'
 
 -- Spellchecking
-vim.opt.spellsuggest= { "best", "10" }
+vim.opt.spellsuggest = { "best", "10" }
 
 -- Backup
 vim.opt.history = 200
@@ -66,7 +66,7 @@ vim.opt.backupdir:remove(".")
 vim.opt.undofile = true
 vim.opt.undodir:remove(".")
 
--- netrw 
+-- netrw
 vim.g.netrw_banner = 0
 vim.g.netrw_liststyle = 3
 vim.g.netrw_list_hide = "(^|ss)zs.S+"
