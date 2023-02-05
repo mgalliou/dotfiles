c280775f6a4397e1797d71df621e0e8f86a81dd5
mgalliou <matthias.galliou@gmail.com>
2023-02-06 00:34:32 +0100

    nvim: add plugin "trouble"

--- neovim/.config/nvim/lua/plugins/tools.lua
+++ neovim/.config/nvim/lua/plugins/tools.lua
@@ -1,27 +1,36 @@
 return {
-	"nvim-neorg/neorg",
-	ft = "norg",
-	build = ":Neorg sync-parsers",
-	opts = {
-		load = {
-			["core.defaults"] = {},
-			["core.norg.dirman"] = {
-				config = {
-					workspaces = {
-						work = "~/notes/work",
-						home = "~/notes/home",
+	{
+		"folke/trouble.nvim",
+		-- TODO: configure
+		opts = {
+			use_diagnostic_signs = true
+		}
+	},
+	{
+		"nvim-neorg/neorg",
+		ft = "norg",
+		build = ":Neorg sync-parsers",
+		opts = {
+			load = {
+				["core.defaults"] = {},
+				["core.norg.dirman"] = {
+					config = {
+						workspaces = {
+							work = "~/notes/work",
+							home = "~/notes/home",
+						},
 					},
 				},
+				["core.norg.concealer"] = {},
+				["core.norg.completion"] = {
+					config = { engine = "nvim-cmp" },
+				},
+				["core.integrations.nvim-cmp"] = {},
 			},
-			["core.norg.concealer"] = {},
-			["core.norg.completion"] = {
-				config = { engine = "nvim-cmp" },
-			},
-			["core.integrations.nvim-cmp"] = {},
+		},
+		keys = {
+			{ "<leader>nww", "<cmd>Neorg workspace work<CR>", desc = "Neorg work workspace" },
+			{ "<leader>njt", "<cmd>Neorg journal today<CR>", desc = "Neorg today journal" }
 		},
 	},
-	keys = {
-		{ "<leader>nww", "<cmd>Neorg workspace work<CR>", desc = "Neorg work workspace" },
-		{ "<leader>njt", "<cmd>Neorg journal today<CR>", desc = "Neorg today journal" }
-	}
 }
