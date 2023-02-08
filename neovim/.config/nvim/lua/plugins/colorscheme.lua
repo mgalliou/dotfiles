46e799576b1526dc902d3cfb11227f04cc00f3ae
mgalliou <matthias.galliou@gmail.com>
2023-02-08 18:45:38 +0100

    nvim: add fix for noice command line border style with gruvbox

--- neovim/.config/nvim/lua/plugins/colorscheme.lua
+++ neovim/.config/nvim/lua/plugins/colorscheme.lua
@@ -22,7 +22,37 @@ return {
 				dim_inactive = false,
 				transparent_mode = false,
 			})
-			vim.cmd([[colorscheme gruvbox]])
+			-- NOTE: temporary fix for `noice` command line border color
+			-- from "https://github.com/ellisonleao/gruvbox.nvim/issues/160"
+			vim.cmd([[
+			function! FixGruvboxForNoice() abort
+				if g:colors_name == "gruvbox"
+					highlight NoiceCmdlinePopupBorderCmdline guifg=#83a598 guibg=NONE
+					highlight NoiceCmdlinePopupBorderFilter guifg=#83a598 guibg=NONE
+					highlight NoiceCmdlinePopupBorderHelp guifg=#83a598 guibg=NONE
+					highlight NoiceCmdlinePopupBorderInput guifg=#83a598 guibg=NONE
+					highlight NoiceCmdlinePopupBorderLua guifg=#83a598 guibg=NONE
+					highlight NoiceCmdlinePopupBorderSearch guifg=#fabd2f guibg=NONE
+
+					highlight NoiceCmdlineIconCmdline guifg=#83a598 guibg=NONE
+					highlight NoiceCmdlineIconFilter guifg=#83a598 guibg=NONE
+					highlight NoiceCmdlineIconHelp guifg=#83a598 guibg=NONE
+					highlight NoiceCmdlineIconInput guifg=#83a598 guibg=NONE
+					highlight NoiceCmdlineIconLua guifg=#83a598 guibg=NONE
+					highlight NoiceCmdlineIconSearch guifg=#fabd2f guibg=NONE
+				endif
+			endfunction
+
+			augroup ApplyFixGruvboxForNoice
+				autocmd!
+				autocmd ColorScheme * call FixGruvboxForNoice()
+			augroup END
+
+			silent !rm $VIMRUNTIME/colors/*.vim &> /dev/null
+			set background=dark
+			colorscheme gruvbox
+			highlight link GitSignsChange GruvboxYellowSign
+			]])
 		end,
 	}
 }
