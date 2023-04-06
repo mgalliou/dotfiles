fc2710054935ca8103e8d62e5b5f7c747c231a64
mgalliou <matthias.galliou@gmail.com>
2023-04-06 11:03:23 +0200

    nvim: disable ansible-vim

--- neovim/.config/nvim/lua/plugins/ft.lua
+++ neovim/.config/nvim/lua/plugins/ft.lua
@@ -2,7 +2,10 @@ return {
 	{ 'dag/vim-fish', ft = "fish"},
 	{ 'tmux-plugins/vim-tmux', ft = "tmux" },
 	{ 'leafgarland/typescript-vim', ft = "typescript" },
-	{ 'pearofducks/ansible-vim', ft = "yaml" },
+	{
+		'pearofducks/ansible-vim',
+		enabled = false
+	},
 	{ 'towolf/vim-helm' },
 	{ 'vim-scripts/tf2.vim', ft = "cfg" }
 }
