b600545fdb0d69a4914352f67d7d65c79b5b6471
mgalliou <matthias.galliou@gmail.com>
2023-05-16 15:25:24 +0200

    fish: remove bobthefish sourcing

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -3,10 +3,9 @@ if status is-interactive
 
 	source $CONFIG_PATH/fundle.fish
 	source $CONFIG_PATH/env.fish
-	source $CONFIG_PATH/prompt.fish
 	source $CONFIG_PATH/abbr.fish
+	source $CONFIG_PATH/prompt.fish
 	source $CONFIG_PATH/fzf.fish
-	source $CONFIG_PATH/bobthefish.fish
 
 	starship init fish | source
 end
