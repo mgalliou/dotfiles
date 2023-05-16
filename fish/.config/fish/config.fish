88399ed518eff3894c5d3b644264555d860e04e9
mgalliou <matthias.galliou@gmail.com>
2023-05-16 10:02:46 +0200

    fish: migrate to fundle plugin manager

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 if status is-interactive
 	set -l CONFIG_PATH ~/.config/fish
 
+	source $CONFIG_PATH/fundle.fish
 	source $CONFIG_PATH/env.fish
 	source $CONFIG_PATH/prompt.fish
 	source $CONFIG_PATH/abbr.fish
