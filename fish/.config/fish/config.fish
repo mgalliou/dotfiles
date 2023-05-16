a14bdd7df317c012c69cb38bd4950971fab3b8d2
mgalliou <matthias.galliou@gmail.com>
2023-05-16 15:29:57 +0200

    fish: refactor config.fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,15 +1,16 @@
-if status is-interactive
-	set -l CONFIG_PATH ~/.config/fish
-
-	source $CONFIG_PATH/fundle.fish
-	source $CONFIG_PATH/env.fish
-	source $CONFIG_PATH/abbr.fish
-	source $CONFIG_PATH/fzf.fish
-
-	if type -q starship
-		starship init fish | source
-	else
-		source $CONFIG_PATH/prompt.fish
-	end
+if not status is-interactive
+    return
 end
 
+set -l CONFIG_PATH ~/.config/fish
+
+source $CONFIG_PATH/fundle.fish
+source $CONFIG_PATH/env.fish
+source $CONFIG_PATH/abbr.fish
+source $CONFIG_PATH/fzf.fish
+
+if type -q starship
+    starship init fish | source
+else
+    source $CONFIG_PATH/prompt.fish
+end
