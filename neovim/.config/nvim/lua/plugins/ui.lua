43c1ca14896b02e6cc53faab3bea9bd9322879ea
mgalliou <matthias.galliou@gmail.com>
2023-05-24 17:18:51 +0200

    feat(neo-tree): add workaround for flickering; configure behavior and look

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -125,14 +125,22 @@ return {
 			{ "MunifTanjim/nui.nvim", lazy = true }
 		},
 		opts = {
+			-- NOTE: workaround to prevent flickering when changing source
+			git_status_async = false,
 			filesystem = {
+				-- NOTE: workaround to prevent flickering when changing source
+				async_directory_scan = "never",
 				filtered_items = {
 					hide_dotfiles = false,
 					hide_hidden = false,
 					hide_gitignored = false,
 				},
 			},
-			follow_current_file = true
+			source_selector = {
+				winbar = true,
+			},
+			add_blank_line_at_top = true,
+			close_if_last_window = true
 		},
 		-- TODO: add keymaps
 		keys = {
