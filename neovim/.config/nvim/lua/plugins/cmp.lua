660e187bdf9f057905fc9f9c546d81f3aee57a58
mgalliou <matthias.galliou@gmail.com>
2024-01-25 10:53:38 +0100

    feat(nvim): remove command line nvim-cmp completion

--- neovim/.config/nvim/lua/plugins/cmp.lua
+++ neovim/.config/nvim/lua/plugins/cmp.lua
@@ -40,7 +40,6 @@ return {
 		dependencies = {
 			"andersevenrud/cmp-tmux",
 			"hrsh7th/cmp-buffer",
-			"hrsh7th/cmp-cmdline",
 			"hrsh7th/cmp-emoji",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-path",
@@ -116,22 +115,6 @@ return {
 						hl_group = "LspCodeLens",
 					},
 				},
-			})
-
-			cmp.setup.cmdline({ "/", "?" }, {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = {
-					{ name = "buffer" },
-				},
-			})
-
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{ name = "cmdline" },
-				}),
 			})
 		end,
 	},
