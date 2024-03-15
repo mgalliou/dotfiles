e9b74c40a7b9ad83d0bec2ec2a62be0b28476d58
mgalliou <matthias.galliou@gmail.com>
2024-03-15 14:19:28 +0100

    feat(fish): improve ruby PATH configuration

--- fish/.config/fish/env.fish
+++ fish/.config/fish/env.fish
@@ -2,14 +2,18 @@ fish_add_path $HOME/bin
 fish_add_path $HOME/.local/bin
 fish_add_path $HOME/.cargo/bin
 fish_add_path $HOME/.opam/bin
-fish_add_path $HOME/.local/share/gem/ruby/3.2.0/bin
+if type -q ruby
+    fish_add_path $HOME/.local/share/gem/ruby/$(ruby -v | awk '{print $2}')/bin
+end
 fish_add_path $HOME/node_modules/.bin
 fish_add_path $HOME/.krew/bin
 fish_add_path $HOME/go/bin
 fish_add_path -aP $HOME/.local/share/nvim/mason/bin/
 fish_add_path -aP $HOME/.linuxbrew/bin
 fish_add_path -aP /home/linuxbrew/.linuxbrew/bin
-fish_add_path -aP /home/linuxbrew/.linuxbrew/lib/ruby/gems/3.2.0/bin
+if type -q ruby
+    fish_add_path -a /home/linuxbrew/.linuxbrew/lib/ruby/gems/$(ruby -v | awk '{print $2}')/bin
+end
 
 if type -q nvim
     set -x EDITOR nvim
