b7e54b97d906baef28c2c669fdeb7c30d6217e39
mgalliou <matthias.galliou@gmail.com>
2024-05-12 14:38:22 +0200

    fix(nvim): update nvim-web-devicons plugin url

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -19,7 +19,7 @@ return {
 	},
 	{
 		"nvim-lualine/lualine.nvim",
-		dependencies = { "kyazdani42/nvim-web-devicons" },
+		dependencies = { "nvim-tree/nvim-web-devicons" },
 		opts = {
 			options = {
 				theme = "gruvbox",
