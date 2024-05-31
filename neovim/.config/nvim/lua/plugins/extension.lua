61f937d6907e57c3d4c8c48990898ef6903ed504
mgalliou <matthias.galliou@gmail.com>
2024-05-31 12:32:53 +0200

    feat(nvim): update trouble configuration for v3 with suggested keybinds

--- neovim/.config/nvim/lua/plugins/extension.lua
+++ neovim/.config/nvim/lua/plugins/extension.lua
@@ -7,9 +7,36 @@ return {
 			use_diagnostic_signs = true,
 		},
 		keys = {
-			{ "<leader>d", ":TroubleToggle document_diagnostics<CR>", desc = "Trouble" },
-			{ "<leader>D", ":TroubleToggle workspace_diagnostics<CR>", desc = "Trouble (workspace)" },
-			{ "<leader>x", ":TodoTrouble<CR>", desc = "TodoTrouble" },
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
 		},
 	},
 	{
