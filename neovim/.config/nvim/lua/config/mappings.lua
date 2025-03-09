4e1aa20033cc57eda0c8257b11139125f0abc6a3
mgalliou <matthias.galliou@gmail.com>
2025-03-09 11:08:35 +0100

    feat(nvim): update lazy.nvim setup

--- neovim/.config/nvim/lua/config/mappings.lua
+++ neovim/.config/nvim/lua/config/mappings.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
 
 vim.keymap.set("n", "<leader>rl", "<CMD>so $MYVIMRC", { desc = "Reload configuration" })
 vim.keymap.set("n", "<leader>ws", "m`:%s/\\s\\+$//<CR>:let @/=''<CR>``:w<CR>", { desc = "Remove trailing whitespaces" })
