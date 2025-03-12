1dfc1a079f4e4d9db88d29050db012670e7283f1
mgalliou <matthias.galliou@gmail.com>
2025-03-12 15:58:06 +0100

    feat(nvim.which-key): add hydra mode keymap

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -31,7 +31,14 @@ return {
 				function()
 					require("which-key").show({ global = false })
 				end,
-				desc = "Buffer Local Keymaps (which-key)",
+				desc = "Buffer Keymaps (which-key)",
+			},
+			{
+				"<c-w><space>",
+				function()
+					require("which-key").show({ keys = "<c-w>", loop = true })
+				end,
+				desc = "Window Hydra Mode (which-key)",
 			},
 		},
 	},
