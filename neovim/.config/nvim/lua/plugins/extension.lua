17e493da5055d449266317b93174a8105ad5cc09
mgalliou <matthias.galliou@gmail.com>
2025-03-12 17:07:07 +0100

    feat(nvim.trouble): add quickfix navigation keymaps

--- neovim/.config/nvim/lua/plugins/extension.lua
+++ neovim/.config/nvim/lua/plugins/extension.lua
@@ -37,6 +37,36 @@ return {
 				"<cmd>Trouble qflist toggle<cr>",
 				desc = "Quickfix List (Trouble)",
 			},
+			{
+				"[q",
+				function()
+					if require("trouble").is_open() then
+						---@diagnostic disable-next-line: missing-parameter, missing-fields
+						require("trouble").prev({ skip_groups = true, jump = true })
+					else
+						local ok, err = pcall(vim.cmd.cprev)
+						if not ok then
+							vim.notify(err, vim.log.levels.ERROR)
+						end
+					end
+				end,
+				desc = "Previous Trouble/Quickfix Item",
+			},
+			{
+				"]q",
+				function()
+					if require("trouble").is_open() then
+						---@diagnostic disable-next-line: missing-parameter, missing-fields
+						require("trouble").next({ skip_groups = true, jump = true })
+					else
+						local ok, err = pcall(vim.cmd.cnext)
+						if not ok then
+							vim.notify(err, vim.log.levels.ERROR)
+						end
+					end
+				end,
+				desc = "Next Trouble/Quickfix Item",
+			},
 		},
 	},
 	{
