44acbfb819e3bc4fa7d7cb89bee9e2755fd4eb97
mgalliou <matthias.galliou@gmail.com>
2025-03-12 15:09:50 +0100

    feat(nvim): update mappings

--- neovim/.config/nvim/lua/config/mappings.lua
+++ neovim/.config/nvim/lua/config/mappings.lua
@@ -1,9 +1,11 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.keymap.set("n", "<leader>rl", "<CMD>so $MYVIMRC", { desc = "Reload configuration" })
-vim.keymap.set("n", "<leader>ws", "m`:%s/\\s\\+$//<CR>:let @/=''<CR>``:w<CR>", { desc = "Remove trailing whitespaces" })
-vim.keymap.set("n", "<leader>lst", "<CMD> set list!<CR>", { desc = "Toggle list chars" })
-vim.keymap.set("n", "<leader>sc", "<CMD>set spell!<CR>", { desc = "Toggle spellcheck" })
+--vim.keymap.set("n", "<leader>rl", "<CMD>so $MYVIMRC", { desc = "Reload configuration" })
+--vim.keymap.set("n", "<leader>ws", "m`:%s/\\s\\+$//<CR>:let @/=''<CR>``:w<CR>", { desc = "Remove trailing whitespaces" })
+vim.keymap.set("n", "<leader>ul", "<CMD>set list!<CR>", { desc = "Toggle list chars" })
+vim.keymap.set("n", "<leader>us", "<CMD>set spell!<CR>", { desc = "Toggle spellcheck" })
+vim.keymap.set("n", "<leader>uw", "<CMD>set wrap!<CR>", { desc = "Toggle wrap" })
+
 -- TODO: add "save file as root" mapping, or find another (better) solution
 -- TODO: add toggle line number map
