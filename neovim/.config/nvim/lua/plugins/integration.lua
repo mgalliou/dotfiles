7d151f59a50abd0c4c37f1fb02923dface98d880
mgalliou <matthias.galliou@gmail.com>
2025-03-31 15:19:40 +0200

    feat(nvim): lazy load zoxide.vim and vim-fugitive using their commands

--- neovim/.config/nvim/lua/plugins/integration.lua
+++ neovim/.config/nvim/lua/plugins/integration.lua
@@ -6,8 +6,35 @@ end
 
 ---@type LazyPluginSpec[]
 return {
-	{ "tpope/vim-fugitive" },
-	{ "nanotee/zoxide.vim" },
+	{
+		"tpope/vim-fugitive",
+		cmd = {
+			"G",
+			"Git",
+			"Gdiffsplit",
+			"Gvdiffsplit",
+			"Gread",
+			"Gwrite",
+			"Grep",
+			"Glgrep",
+			"Gmove",
+			"GRename",
+			"GDelete",
+			"GRemove",
+			"GBrowse",
+		},
+	},
+	{
+		"nanotee/zoxide.vim",
+		cmd = {
+			"Z",
+			"Lz",
+			"Tz",
+			"Zi",
+			"Lzi",
+			"Tzi",
+		},
+	},
 	{
 		"mickael-menu/zk-nvim",
 		enabled = false,
