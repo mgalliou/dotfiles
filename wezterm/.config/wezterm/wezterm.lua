877e6b49efc43b208ed754ac3439f64ec26dcf71
mgalliou <matthias.galliou@gmail.com>
2025-05-22 18:28:42 +0200

    feat(wezterm): remove window_decorations and hide tabs if only one tab

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,13 +2,14 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "Bold" })
-config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
 config.color_scheme = "Gruvbox dark, medium (base16)"
+config.hide_tab_bar_if_only_one_tab = true
 
 local active_fg = "#ebdbb2"
 local inactive_fg = "#a89984"
 local active_bg = "#282828"
 local inactive_bg = "#333333"
+
 config.colors = {
 	tab_bar = {
 		active_tab = {
