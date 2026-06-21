e7dda9349e48dc8a436fbf4583f02d9c72e5ebdf
mgalliou <matthias.galliou@gmail.com>
2026-06-21 12:09:19 +0200

    refactor(nvim): simplify Godot detection with vim.fs.find

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -5,23 +5,12 @@ require("config.lazy")
 require("config.options")
 require("config.autocmds")
 
--- paths to check for project.godot file
-local paths_to_check = { "/", "/../" }
-local is_godot_project = false
-local godot_project_path = ""
-local cwd = vim.fn.getcwd()
-
--- iterate over paths and check
-for _, value in pairs(paths_to_check) do
-	if vim.uv.fs_stat(cwd .. value .. "project.godot") then
-		is_godot_project = true
-		godot_project_path = cwd .. value
-		break
-	end
-end
+local godot_file = vim.fs.find("project.godot", { upward = true })[1]
+local is_godot_project = godot_file ~= nil
+local godot_project_path = is_godot_project and vim.fs.dirname(godot_file) or ""
 
 -- check if server is already running in godot project path
-local is_server_running = vim.uv.fs_stat(godot_project_path .. "/server.pipe")
+local is_server_running = godot_project_path ~= "" and vim.uv.fs_stat(godot_project_path .. "/server.pipe")
 -- start server, if not already running
 if is_godot_project and not is_server_running then
 	vim.fn.serverstart(godot_project_path .. "/server.pipe")
