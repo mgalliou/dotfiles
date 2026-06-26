ab564e61c837d5f7fb3dae7cc984b0c763027ef5
mgalliou <matthias.galliou@gmail.com>
2026-06-26 15:22:36 +0200

    style(nvim): run stylua

--- neovim/.config/nvim/lua/config/mappings.lua
+++ neovim/.config/nvim/lua/config/mappings.lua
@@ -20,18 +20,30 @@ map("v", ">", ">gv")
 map("n", "<leader>xl", function()
 	local ok, err = pcall(function()
 		local loclist = vim.fn.getloclist(0, { winid = 0 })
-		if loclist.winid ~= 0 then vim.cmd.lclose() else vim.cmd.lopen() end
+		if loclist.winid ~= 0 then
+			vim.cmd.lclose()
+		else
+			vim.cmd.lopen()
+		end
 	end)
-	if not ok then vim.notify(err, vim.log.levels.ERROR) end
+	if not ok then
+		vim.notify(err, vim.log.levels.ERROR)
+	end
 end, { desc = "Location List" })
 
 -- quickfix list
 map("n", "<leader>xq", function()
 	local ok, err = pcall(function()
 		local qflist = vim.fn.getqflist({ winid = 0 })
-		if qflist.winid ~= 0 then vim.cmd.cclose() else vim.cmd.copen() end
+		if qflist.winid ~= 0 then
+			vim.cmd.cclose()
+		else
+			vim.cmd.copen()
+		end
 	end)
-	if not ok then vim.notify(err, vim.log.levels.ERROR) end
+	if not ok then
+		vim.notify(err, vim.log.levels.ERROR)
+	end
 end, { desc = "Quickfix List" })
 
 -- diagnostics
