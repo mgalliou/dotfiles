1f06a62b47dfa004e401210c835d078c4ecf8094
mgalliou <matthias.galliou@gmail.com>
2026-06-26 15:12:51 +0200

    feat(nvim): rewrite treesitter config for Neovim 0.12 API

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -5,109 +5,124 @@ return {
 		branch = "main",
 		build = ":TSUpdate",
 		event = { "VeryLazy", unpack(Utils.buf_events) },
-		cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
-		keys = {
-			{ "<C-space>", desc = "Increment Selection" },
-			{ "<BS>", desc = "Decrement Selection", mode = "x" },
-		},
-		---@module "nvim-treesitter"
-		---@type TSConfig
-		---@diagnostic disable-next-line: missing-fields
+		cmd = { "TSInstall", "TSUpdate", "TSLog", "TSUninstall" },
 		opts = {
 			auto_install = true,
-			highlight = { enable = true },
-			indent = { enable = true },
 			ensure_installed = {
-				"gitcommit",
+				"bash",
 				"diff",
+				"fish",
+				"gitcommit",
+				"json",
+				"lua",
+				"luadoc",
+				"luap",
 				"markdown",
 				"markdown_inline",
-				"bash",
-			},
-			incremental_selection = {
-				enable = true,
-				keymaps = {
-					init_selection = "<C-space>",
-					node_incremental = "<C-space>",
-					scope_incremental = false,
-					node_decremental = "<BS>",
-				},
-			},
-			textobjects = {
-				select = {
-					enable = true,
-					lookahead = true,
-					keymaps = {
-						["ak"] = { query = "@block.outer", desc = "around block" },
-						["ik"] = { query = "@block.inner", desc = "inside block" },
-						["ac"] = { query = "@class.outer", desc = "around class" },
-						["ic"] = { query = "@class.inner", desc = "inside class" },
-						["a?"] = { query = "@conditional.outer", desc = "around conditional" },
-						["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
-						["af"] = { query = "@function.outer", desc = "around function " },
-						["if"] = { query = "@function.inner", desc = "inside function " },
-						["ao"] = { query = "@loop.outer", desc = "around loop" },
-						["io"] = { query = "@loop.inner", desc = "inside loop" },
-						["aa"] = { query = "@parameter.outer", desc = "around argument" },
-						["ia"] = { query = "@parameter.inner", desc = "inside argument" },
-					},
-				},
-				move = {
-					enable = true,
-					goto_previous_start = {
-						["[f"] = "@function.outer",
-						["[c"] = "@class.outer",
-						["[a"] = "@parameter.inner",
-						["[v"] = "@assignment.outer",
-					},
-					goto_next_start = {
-						["]f"] = "@function.outer",
-						["]c"] = "@class.outer",
-						["]a"] = "@parameter.inner",
-						["]v"] = "@assignment.outer",
-					},
-					goto_previous_end = {
-						["[F"] = "@function.outer",
-						["[C"] = "@class.outer",
-						["[A"] = "@parameter.inner",
-					},
-					goto_next_end = {
-						["]F"] = "@function.outer",
-						["]C"] = "@class.outer",
-						["]A"] = "@parameter.inner",
-					},
-				},
-				swap = {
-					enable = true,
-					swap_previous = {
-						["<leader>[f"] = "@function.outer",
-						["<leader>[c"] = "@class.outer",
-						["<leader>[a"] = "@parameter.inner",
-						["<leader>[v"] = "@assignment.outer",
-					},
-					swap_next = {
-						["<leader>]f"] = "@function.outer",
-						["<leader>]c"] = "@class.outer",
-						["<leader>]a"] = "@parameter.inner",
-						["<leader>]v"] = "@assignment.outer",
-					},
-				},
+				"python",
+				"query",
+				"regex",
+				"rust",
+				"toml",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
 			},
 		},
+		config = function(_, opts)
+			require("nvim-treesitter").setup(opts)
+
+			vim.api.nvim_create_autocmd("FileType", {
+				desc = "Start treesitter highlighting",
+				group = vim.api.nvim_create_augroup("treesitter_highlight", { clear = true }),
+				callback = function()
+					pcall(vim.treesitter.start)
+				end,
+			})
+		end,
 	},
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		branch = "main",
-		event = { "VeryLazy" },
-		-- TODO: finish configuring keymaps
-		-- config = function()
-		-- 	vim.keymap.set("n", "<leader>]a", function()
-		-- 		require("nvim-treesitter-textobjects.swap").swap_next("@parameter.inner")
-		-- 	end)
-		-- 	vim.keymap.set("n", "<leader>[a", function()
-		-- 		require("nvim-treesitter-textobjects.swap").swap_previous("@parameter.outer")
-		-- 	end)
-		-- end,
+		init = function()
+			vim.g.no_plugin_maps = true
+		end,
+		opts = {
+			select = {
+				lookahead = true,
+				selection_modes = {
+					["@parameter.outer"] = "v",
+					["@function.outer"] = "V",
+					["@class.outer"] = "V",
+				},
+			},
+			move = {
+				set_jumps = true,
+			},
+			swap = {},
+		},
+		config = function(_, opts)
+			require("nvim-treesitter-textobjects").setup(opts)
+
+			local select = require("nvim-treesitter-textobjects.select").select_textobject
+			for _, spec in ipairs({
+				{ "ak", "@block.outer", "around block" },
+				{ "ik", "@block.inner", "inside block" },
+				{ "ac", "@class.outer", "around class" },
+				{ "ic", "@class.inner", "inside class" },
+				{ "a?", "@conditional.outer", "around conditional" },
+				{ "i?", "@conditional.inner", "inside conditional" },
+				{ "af", "@function.outer", "around function" },
+				{ "if", "@function.inner", "inside function" },
+				{ "ao", "@loop.outer", "around loop" },
+				{ "io", "@loop.inner", "inside loop" },
+				{ "aa", "@parameter.outer", "around argument" },
+				{ "ia", "@parameter.inner", "inside argument" },
+			}) do
+				vim.keymap.set({ "x", "o" }, spec[1], function()
+					select(spec[2])
+				end, { desc = spec[3] })
+			end
+
+			local move = require("nvim-treesitter-textobjects.move")
+			for _, spec in ipairs({
+				{ "[f", move.goto_previous_start, "@function.outer", "prev function start" },
+				{ "]f", move.goto_next_start, "@function.outer", "next function start" },
+				{ "[c", move.goto_previous_start, "@class.outer", "prev class start" },
+				{ "]c", move.goto_next_start, "@class.outer", "next class start" },
+				{ "[a", move.goto_previous_start, "@parameter.inner", "prev param" },
+				{ "]a", move.goto_next_start, "@parameter.inner", "next param" },
+				{ "[v", move.goto_previous_start, "@assignment.outer", "prev assignment" },
+				{ "]v", move.goto_next_start, "@assignment.outer", "next assignment" },
+				{ "[F", move.goto_previous_end, "@function.outer", "prev function end" },
+				{ "]F", move.goto_next_end, "@function.outer", "next function end" },
+				{ "[C", move.goto_previous_end, "@class.outer", "prev class end" },
+				{ "]C", move.goto_next_end, "@class.outer", "next class end" },
+				{ "[A", move.goto_previous_end, "@parameter.inner", "prev param end" },
+				{ "]A", move.goto_next_end, "@parameter.inner", "next param end" },
+			}) do
+				vim.keymap.set({ "n", "x", "o" }, spec[1], function()
+					spec[2](spec[3], "textobjects")
+				end, { desc = spec[4] })
+			end
+
+			local swap = require("nvim-treesitter-textobjects.swap")
+			for _, spec in ipairs({
+				{ "<leader>[f", swap.swap_previous, "@function.outer", "swap prev function" },
+				{ "<leader>]f", swap.swap_next, "@function.outer", "swap next function" },
+				{ "<leader>[c", swap.swap_previous, "@class.outer", "swap prev class" },
+				{ "<leader>]c", swap.swap_next, "@class.outer", "swap next class" },
+				{ "<leader>[a", swap.swap_previous, "@parameter.inner", "swap prev arg" },
+				{ "<leader>]a", swap.swap_next, "@parameter.inner", "swap next arg" },
+				{ "<leader>[v", swap.swap_previous, "@assignment.outer", "swap prev assignment" },
+				{ "<leader>]v", swap.swap_next, "@assignment.outer", "swap next assignment" },
+			}) do
+				vim.keymap.set("n", spec[1], function()
+					spec[2](spec[3])
+				end, { desc = spec[4] })
+			end
+		end,
 	},
 	{
 		"mtrajano/tssorter.nvim",
