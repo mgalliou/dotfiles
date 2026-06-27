9a77894b74c0b9555e1939f9df2d993e39693f8e
mgalliou <matthias.galliou@gmail.com>
2026-06-27 12:16:37 +0200

    feat(wezterm): use GruvboxDark color scheme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.font = wezterm.font("JetBrainsMonoNL Nerd Font", { weight = "Bold" })
-config.color_scheme = "Gruvbox dark, medium (base16)"
+config.color_scheme = "GruvboxDark"
 config.hide_tab_bar_if_only_one_tab = true
 
 local active_fg = "#ebdbb2"
